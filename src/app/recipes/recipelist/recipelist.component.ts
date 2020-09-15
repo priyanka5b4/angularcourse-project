c749ae9b558f89761da0d5285fbf1de5724ae011
lakss <Lakshmi_S@mentor.com>
2020-09-15 18:04:37 +0530

    basic setup

--- src/app/recipes/recipelist/recipelist.component.ts
+++ src/app/recipes/recipelist/recipelist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model'
 
 @Component({
   selector: 'app-recipelist',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipelist.component.css']
 })
 export class RecipelistComponent implements OnInit {
-
+   recipes:Recipe[] =[
+     new Recipe('A test Recipe', 'Just a small  test', 'https://images.immediate.co.uk/production/volatile/sites/2/2016/02/20501.jpg?quality=90&crop=1px%2C328px%2C597px%2C257px&resize=597%2C254')
+   ];
   constructor() { }
 
   ngOnInit(): void {
